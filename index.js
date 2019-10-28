4d6b57a7670e9bd620e67535c9d404bd8eed2092
CindyChenx <chen_cx0128@hotmail.com>
2019-10-28 09:59:35 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+  const timeSplit = timeString.split(":");
+  const hours = parseInt(timeSplit[0], 10); 
+  if (hours < 12) {
+    return "Good Morning";
+  } else if (hours == 12 || hours <= 17) {
+    return "Good Afternoon";
+  } else if (hours > 17) {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  let greeting = document.getElementById("greeting");
+  greeting.innerText = msg;
+}
